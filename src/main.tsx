b2264290a793cfcf2eb766ca4098bfcb0c1388b9
Aman Meherally <amanmeherally@gmail.com>
2026-02-04 17:42:00 -0500

    feat: posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { PostHogProvider } from "posthog-js/react";
 
 import "./globals.css";
 
+const options = {
+  api_host: "/translate",
+  ui_host: "https://us.posthog.com",
+  defaults: "2025-11-30",
+} as const;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      options={options}
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 );
